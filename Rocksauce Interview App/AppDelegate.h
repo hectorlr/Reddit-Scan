e34db97665bd31be4e3c117e3385be112b733d1e
Hector Rodriguez <hector.rodriguez@outlook.com>
2014-03-01 13:54:58 -0700

    Initial Commit

---
+++ Rocksauce Interview App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rocksauce Interview App
+//
+//  Created by Hector Rodriguez on 3/1/14.
+//  Copyright (c) 2014 HectorRodriguz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
